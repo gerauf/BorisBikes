4a9de1c6a82778e25baf084296d8001f39305051
Ruta Baltiejute <Ruta@Rutas-MacBook-Pro.local>
2016-04-13 11:57:12 +0100

    End of lesson 12

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,8 +5,10 @@ describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
   it "gets a bike" do
-    bicycle = subject.release_bike
-    expect(bicycle).to be_working
+    bike = Bike.new
+    subject.dock(bike)
+    subject.release_bike
+    expect(bike).to be_working
   end
 
   it { is_expected.to respond_to(:dock).with(1).argument }
@@ -22,4 +24,15 @@ describe DockingStation do
     expect(subject.bike).to eq bike
   end
 
+    describe '#release_bike' do
+      it "releases a bike" do
+        bike = Bike.new
+        subject.dock(bike)
+        expect(subject.release_bike).to eq bike
+      end
+
+    it "raises error when no bike available" do
+      expect { subject.release_bike}.to raise_error("No bike in dock!")
+    end
+  end
 end
