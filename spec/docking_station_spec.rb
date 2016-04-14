b3a2028fc35e7c3f80f468cfce6c563f0bdea443
Elia Bardashevich <ebardashevich@gmail.com>
2016-04-14 19:23:41 +0100

    updated challenge 18 half way

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -19,25 +19,29 @@ describe DockingStation do
 			end
 
 			describe '#release_bike' do
-					it 'releases a bike' do
-						bike = Bike.new
-						subject.dock(bike)
-						expect(subject.release_bike).to eq bike
-          #  expect{subject.release_bike}.to raise_error("bike is broken")
-					end
+				it 'releases a bike' do
+					bike = Bike.new
+					subject.dock(bike)
+					expect(subject.release_bike(bike)).to eq bike
+        end
+
+				it "raises an erro when the bike is broken" do
+         expect {subject.release_bike(bike)}.to raise_error("bike is broken")
+				end
 
 
+				it "has a working bike" do
+					bike = Bike.new
+					subject.dock(bike)
+         	expect(subject.release_bike(bike).working?).to eq true
+					#expect(subject.release_bike.working?).to eq false
+			  end
+				it "raises exception if no bikes in docking station" do
+					bike = Bike.new
+					expect {subject.release_bike(bike)}.to raise_error("No bikes")
+				end
+			end
 
-					end
-					it "has a working bike" do
-						bike = Bike.new
-						subject.dock(bike)
-           	expect(subject.release_bike.working?).to eq true
-						#expect(subject.release_bike.working?).to eq false
-					end
-					it "raises exception if no bikes in docking station" do
-						expect {subject.release_bike}.to raise_error("No bikes")
-					end
 
 
 			describe '#dock' do
